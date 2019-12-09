515ac76293485a2df909fe925def8fdd8af4da3b
MichaeliusDenModige <s143049@student.dtu.dk>
2019-12-09 16:08:02 +0100

    DONEZO

--- Assets/MeinScript.cs
+++ Assets/MeinScript.cs
@@ -1,18 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class MeinScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Sprite[] Presentation;
+    private SpriteRenderer spriteRenderer;
+    private VideoPlayer videoPlayer;
+
+    private int index = 0;
+
+    private void Awake()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        videoPlayer = Camera.main.GetComponent<VideoPlayer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseDown()
     {
-        
+        if (videoPlayer.isPlaying)
+            videoPlayer.Stop();
+
+        index++;
+        spriteRenderer.sprite = Presentation[index];
+
+        if (spriteRenderer.sprite.name == "VIDEO")
+            PlayVideo();
+    }
+
+    private void PlayVideo()
+    {
+        videoPlayer.Play();
     }
 }
